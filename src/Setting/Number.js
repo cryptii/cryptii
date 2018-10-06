cc34351cdf60f4fd0135ee7377f4f671fe96e1b5
Fränz Friederes <fraenz@frieder.es>
2018-10-06 22:00:44 +0200

    Adds value rotation to NumberSetting, refs #26

--- src/Setting/Number.js
+++ src/Setting/Number.js
@@ -7,14 +7,17 @@ import Setting from '../Setting'
  */
 export default class NumberSetting extends Setting {
   /**
-   * Setting constructor
+   * Constructor
    * @param {string} name
-   * @param {Object} spec
+   * @param {object} spec
    * @param {mixed} [spec.options] Setting options
    * @param {boolean} [spec.options.integer=false] Wether to use integer values
    * @param {?number} [spec.options.step=1] Step size
    * @param {?number} [spec.options.min=null] Minimum value (inclusive)
    * @param {?number} [spec.options.max=null] Maximum value (exclusive)
+   * @param {?boolean} [spec.options.rotate=true] Wether the value should rotate
+   * when stepping over limits. Rotation can only be enabled when both min and
+   * max values are defined.
    */
   constructor (name, spec) {
     super(name, spec)
@@ -25,6 +28,8 @@ export default class NumberSetting extends Setting {
     this._step = options.step || 1
     this._min = options.min || null
     this._max = options.max || null
+    this._rotate =
+      (options.rotate || true) && this._min !== null && this._max !== null
   }
 
   /**
@@ -64,7 +69,7 @@ export default class NumberSetting extends Setting {
   }
 
   /**
-   * Step up or down value until finding the next valid one.
+   * Step up or down value and repeat the process until finding a valid one.
    * @param {number} step Relative step size
    * @param {number} [maxTries=100] Max number of tries to find a valid value
    * @return {?number} Resulting value or null if unable to find
@@ -74,21 +79,26 @@ export default class NumberSetting extends Setting {
     let tries = 0
     let valueFound = false
 
-    // step until a valid value is found
     while (
+      // step value until a valid one is found or until max tries is reached
       !valueFound &&
       tries++ < maxTries &&
+      // stop when reaching limits with rotation disabled
       // eslint-disable-next-line no-unmodified-loop-condition
-      (step > 0 || value !== this._min) &&
+      (this._rotate || step > 0 || value !== this._min) &&
       // eslint-disable-next-line no-unmodified-loop-condition
-      (step < 0 || value !== this._max)
+      (this._rotate || step < 0 || value !== this._max)
     ) {
       // add step to value
       value += step
 
-      // check bounds
-      value = this._max !== null ? Math.min(value, this._max) : value
-      value = this._min !== null ? Math.max(value, this._min) : value
+      // rotate to min or max value when reaching limits
+      if (this._rotate && value > this._max && this._rotate) {
+        value = this._min
+      } else if (this._rotate && value < this._min && this._rotate) {
+        // step is added during rotation due to max being defined exclusive
+        value = this._max + step
+      }
 
       // validate value
       valueFound = this.validateValue(value) === true
@@ -130,7 +140,7 @@ export default class NumberSetting extends Setting {
    */
   setMin (min) {
     this._min = min
-    return this
+    return this.revalidateValue()
   }
 
   /**
@@ -148,6 +158,25 @@ export default class NumberSetting extends Setting {
    */
   setMax (max) {
     this._max = max
+    return this.revalidateValue()
+  }
+
+  /**
+   * Returns wether the value should rotate when stepping over limits.
+   * @return {boolean}
+   */
+  isRotate () {
+    return this._rotate
+  }
+
+  /**
+   * Sets wether the value should rotate when stepping over limits. Rotation can
+   * only be enabled when both min and max values are defined.
+   * @param {boolean} rotate Wether rotation is enabled
+   * @return {NumberSetting} Fluent interface
+   */
+  setRotate (rotate) {
+    this._rotate = rotate && this._min !== null && this._max !== null
     return this
   }
 
