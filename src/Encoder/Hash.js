9328aac7ec0dfbbeed20167ae22eaf9a5099af5f
Fränz Friederes <fraenz@frieder.es>
2024-10-27 21:06:37 +0100

    Fix an issue where encoders Hash function and HMAC are not available, refs #153

--- src/Encoder/Hash.js
+++ src/Encoder/Hash.js
@@ -22,7 +22,6 @@ const algorithms = [
     label: 'SHA-1',
     blockSize: 64,
     browserAlgorithm: 'SHA-1',
-    browserExceptions: ['ie=11', 'edge'],
     nodeAlgorithm: 'sha1'
   },
   {
@@ -44,7 +43,6 @@ const algorithms = [
     label: 'SHA-512',
     blockSize: 128,
     browserAlgorithm: 'SHA-512',
-    browserExceptions: ['ie=11'],
     nodeAlgorithm: 'sha512'
   }
 ]
@@ -151,9 +149,6 @@ export default class HashEncoder extends Encoder {
 
       // Browser environment
       if (!isNode && algorithm.browserAlgorithm !== undefined) {
-        if (algorithm.browserExceptions !== undefined) {
-          return !EnvUtil.match.apply(EnvUtil, algorithm.browserExceptions)
-        }
         return true
       }
 
